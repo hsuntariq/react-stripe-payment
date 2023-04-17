62165ace2f4123919e57f8f1d81236df5d520088
hsuntarq <hsuntariq@gmail.com>
2023-04-17 18:19:41 +0500

    styled the front end

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -5,8 +5,8 @@ import { CartContext } from '../context/cartContext'
 import '../css/styles.css'
 import { products } from '../data/productsData'
 const Home = () => {
-    const data = useContext(CartContext); 
-    console.log(data.items);
+    const cart = useContext(CartContext); 
+    console.log(cart.items);
 return (
     <>
         <Row className="justify-content-center my-card mt-3">
@@ -14,10 +14,20 @@ return (
                 const { id, title, price } = item;
                 return(
                     <>
-                        <Col key={index} lg={3} className="shadow p-3 justifi-content-center align-items-center d-flex flex-column">
+                        <Col key={index} lg={3} md={4} sm={6} className="shadow p-3  d-flex flex-column">
                             <h4>{title}</h4>
-                            <h6>${price}</h6>
-                            <Button onClick={()=>data.addOneToCart(id)} variant="success">Add to Cart</Button>
+                            <Row className="align-items-center">
+                                <Col sm={6}>
+                                    <h6>${price}</h6>
+                                    <p className="text-secondary">
+                                        In Cart: {cart.getTotalQuantity(id)}
+                                    </p>
+                                </Col>
+                                <Col className="d-flex justify-content-between mb-3" sm={6}>
+                                    <Button onClick={()=>cart.addOneToCart(id)} variant="info">+</Button>
+                                    <Button onClick={()=>cart.removeOneFromCart(id)} variant="danger">-</Button>
+                                </Col>
+                            </Row>
                         </Col>
                     </>
                 )
